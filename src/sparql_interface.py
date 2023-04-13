9d2cd981fbfa993960f5f20935868eb1e8bce0b6
Ines Blin <iblin431@outlook.fr>
2023-04-13 18:42:39 +0200

    updating sparql interface to make the search work

--- src/sparql_interface.py
+++ src/sparql_interface.py
@@ -1,65 +1,88 @@
 """
 #TO DO: add documentation on this script
 """
+import os
+import yaml
 import urllib.request
 import pandas as pd
 from SPARQLWrapper import SPARQLWrapper, RDFXML
-from settings import AGENT
+from settings import AGENT, FOLDER_PATH
+from src.interface import Interface
 
-class SPARQLInterface:
+DEFAULT_PRED = \
+    ["http://www.w3.org/1999/02/22-rdf-syntax-ns#type",
+     "http://dbpedia.org/ontology/date",
+     "http://dbpedia.org/ontology/startDate",
+     "http://dbpedia.org/ontology/endDate",
+     "http://dbpedia.org/property/birthDate",
+     "http://dbpedia.org/property/deathDate"]
+    
+with open(os.path.join(FOLDER_PATH, "dataset-config", "dbpedia.yaml"),
+          encoding='utf-8') as file:
+    dbpedia_dataset_config = yaml.load(file, Loader=yaml.FullLoader)
+
+class SPARQLQuery:
     """
-    #TO DO: add documentation on this script
+    Creating SPARQL queries based on templates + params
     """
-    def __init__(self, sparql_endpoint: str = "http://dbpedia.org/sparql",
-                 agent: str = AGENT):
-        self.sparql = SPARQLWrapper(sparql_endpoint, agent=agent)
+    def __init__(self):
         self.query_template = self._set_query_template()
-
+    
     def _set_query_template(self) -> str:
         query = """
-        PREFIX dbo: <http://dbpedia.org/ontology/>
-        PREFIX rdfs: <http://www.w3.org/2000/01/rdf-schema#>
-        PREFIX rdf: <http://www.w3.org/1999/02/22-rdf-syntax-ns#>
         CONSTRUCT {
-            ?s1 ?p1 ?o1 .
-            ?s1 rdf:type ?type1 .
+            ?s ?p ?o .
         }
-        WHERE {   
-        VALUES ?o1 { <[1]> } 
-        ?s1 ?p1 ?o1 .
-        ?s1 rdfs:label ?s1Label .
-        FILTER langMatches( lang(?s1Label), "en" ) .
-        FILTER(?p1 NOT IN (dbo:wikiPageWikiLink)) .
-        [2]
-        ?s1 rdf:type ?type1 .
+        WHERE {
+        <VALUES-unique-subject>
+        <VALUES-unique-predicate>
+        <VALUES-unique-object>
+        ?s ?p ?o .
             }
         """
+        return query
+    
+    def __call__(self, params: dict[str, str]):
+        query = self.query_template
+        for name, abbr in [("subject", "s"), ("predicate", "p"), ("object", "o")]:
+            if name in params and params[name]:
+                query = query.replace(
+                    f"<VALUES-unique-{name}>",
+                    "VALUES ?" + abbr + " { <" + params[name] + "> } "
+                )
+            else:
+                query = query.replace(f"<VALUES-unique-{name}>", "")
 
         return query
 
-    def _get_predicate_filter(self, predicate: list[str]) -> str:
-        return f"FILTER(?p1 IN ({', '.join(predicate)}))"
 
-    def _format_template(self, node: str, predicate: list) -> str:
-        final_query = self.query_template
-        final_query = final_query.replace("[1]", node)
+class SPARQLInterface(Interface):
+    """
+    #TO DO: add documentation on this script
+    """
+    def __init__(self, dataset_config: dict = dbpedia_dataset_config,
+                 dates: list[str] = [None, None], default_pred: list[str] = DEFAULT_PRED,
+                 filter_kb: bool = 1, sparql_endpoint: str = "http://dbpedia.org/sparql",
+                 agent: str = AGENT):
+        Interface.__init__(self, dataset_config=dataset_config, dates=dates,
+                           default_pred=default_pred, filter_kb=filter_kb)           
+        self.sparql = SPARQLWrapper(sparql_endpoint, agent=agent)
+        self.sparql_query = SPARQLQuery()
+    
+    def get_triples(self, params: dict[str, str]):
+        query = self.sparql_query(params=params)
+        triples = self.call_endpoint(query=query)
+        return triples
 
-        filter_type = "" if not predicate else self._get_predicate_filter(predicate=predicate)
-        final_query = final_query.replace("[2]", filter_type)
-        return final_query
-
-    def __call__(self, node: str = '', predicate: list[str] = [], query: str = '') -> pd.core.frame.DataFrame:
-        if not query:
-            query = self._format_template(node=node, predicate=predicate)
+    def call_endpoint(self, query: str) -> pd.core.frame.DataFrame:
         proxy_support = urllib.request.ProxyHandler({})
         opener = urllib.request.build_opener(proxy_support)
         urllib.request.install_opener(opener)
         self.sparql.setQuery(query)
         self.sparql.setReturnFormat(RDFXML)
         results = self.sparql.query().convert()
-        return pd.DataFrame({"subject": [row[0] for row in results],
-                             "predicate": [row[1] for row in results],
-                             "object": [row[2] for row in results]}).drop_duplicates()
+        return [(str(triple[0]), str(triple[1]), str(triple[2])) \
+            for triple in list(set(results))]
 
 
 if __name__ == '__main__':
@@ -71,6 +94,8 @@ if __name__ == '__main__':
     # "https://api.triplydb.com/datasets/DBpedia-association/dbpedia/services/dbpedia/sparql"
 
     interface = SPARQLInterface()
-    df = interface(node=NODE, predicate=PREDICATE)
-    df.to_csv("sparql.csv")
-    print(df)
+    triples = interface.run_request(params={"subject": NODE}, filter_pred=[], filter_keep=False)
+    print(triples)
+    # df = interface(node=NODE, predicate=PREDICATE)
+    # df.to_csv("sparql.csv")
+    # print(df)
